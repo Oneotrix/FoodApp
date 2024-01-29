80a51cf4d825b06cf0200de429b2d5fd295b480f
m5hut <m5555hutov@gmail.com>
2024-01-29 13:37:29 +0300

    Add fragment

--- app/src/main/java/com/dipom/food/MainActivity.kt
+++ app/src/main/java/com/dipom/food/MainActivity.kt
@@ -2,10 +2,23 @@ package com.dipom.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.dipom.food.databinding.ActivityMainBinding
+import com.dipom.food.mvp.ui.MealsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, MealsFragment.newInstance())
+            .commit()
+
     }
 }
---
+++ app/src/main/java/com/dipom/food/mvp/ui/MealsFragment.kt
@@ -0,0 +1,29 @@
+package com.dipom.food.mvp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dipom.food.databinding.FragmentMealsBinding
+
+class MealsFragment: Fragment() {
+
+    private var _binding: FragmentMealsBinding? = null
+    private val binding: FragmentMealsBinding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMealsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        fun newInstance() = MealsFragment()
+    }
+}
